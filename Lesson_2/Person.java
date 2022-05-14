fb9b4305b566aa8ac5e8dcda08a1af173362f21c
alemooned <yellow.code@yandex.ru>
2022-05-14 22:21:26 +0300

    Person HW2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	char sex = "M";
+	String name = "John";
+	float growth = 180f;
+	float weight = 50.5f;
+	int age = 20;	
+	
+	void move() {
+		System.out.println("moving");
+	}	
+	
+	void seat() {
+		System.out.println("seating");
+	}
+	
+	void run() {
+		System.out.println("running");
+	}
+	
+	void speak() {
+		System.out.println("speaking");
+	}
+	
+	void learnJava() {
+		System.out.println("learning Java");
+	}        
+}
