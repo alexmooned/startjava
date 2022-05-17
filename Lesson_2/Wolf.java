14232c949f5247931d6c658a7edfddd7726f52a4
alemooned <yellow.code@yandex.ru>
2022-05-17 22:14:18 +0300

    Wolf HW2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,30 @@
+public class Wolf {
+	
+	char sex;
+	String nickName;
+	float weight;
+	float height;
+	int age;
+	String color;
+	
+	void move() {
+		System.out.println("moving");
+	}	
+	
+	void sit() {
+		System.out.println("seating");
+	}
+	
+	void run() {
+		System.out.println("running");
+	}
+	
+	void howl() {
+		System.out.println("howling");
+	}
+	
+	void hunt() {
+		System.out.println("hunting");
+		
+	}        
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,27 @@
+public class WolfTest {
+	
+    public static void main(String[] args) {
+		
+		Wolf greyWolf = new Wolf();
+		greyWolf.sex = 'M';
+		greyWolf.nickName = "Grey Wolf";
+		greyWolf.weight = 15.5f;
+		greyWolf.height = 1.5f;
+		greyWolf.age = 2;
+		greyWolf.color = "Grey";
+		
+		System.out.println("sex: " + greyWolf.sex);
+		System.out.println("nickName: " + greyWolf.nickName);
+		System.out.println("weight: " + greyWolf.weight);
+		System.out.println("height: " + greyWolf.height);
+		System.out.println("age: " + greyWolf.age);
+		System.out.println("color: " + greyWolf.color);
+		
+		greyWolf.move();
+		greyWolf.sit();
+		greyWolf.run();
+		greyWolf.howl();
+		greyWolf.hunt();	
+		
+	}
+}
