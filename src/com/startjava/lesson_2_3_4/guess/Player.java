5e8d441e1c9cdab6e65f78aac1a84f0377ec727d
alemooned <yellow.code@yandex.ru>
2022-06-16 22:11:39 +0300

    HW_4

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,8 +1,14 @@
+package com.startjava.lesson_2_3_4.guess;
+
 public class Player {
 
     String name;
     
     public Player(String name) {
             this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
